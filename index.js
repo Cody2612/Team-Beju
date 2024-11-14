14e558bf62bee3f609c8d58f1f73c1a41f741cfd
Cody2612 <b.cody_26@yahoo.com>
2024-11-14 15:45:07 +0000

    be able to querry the cloud server and receive data

---
+++ index.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+// Replace the uri string with your connection string.
+const uri =
+  `mongodb+srv://bogdancodau1:CP9goF5suVuEuhQr@0-baeju-mongotest.9idso.mongodb.net/`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
